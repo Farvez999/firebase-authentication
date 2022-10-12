6323e7d8e00aef4c14b3acffa459b22a714b0561
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-10-13 01:01:03 +0600

    google popup sign in

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBsVnbOlruAeyfyE4mSGdwN_fFpQqNhCNg",
+    authDomain: "fir-authentication-6f525.firebaseapp.com",
+    projectId: "fir-authentication-6f525",
+    storageBucket: "fir-authentication-6f525.appspot.com",
+    messagingSenderId: "189992510400",
+    appId: "1:189992510400:web:4342e7cba004af383bde25"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
